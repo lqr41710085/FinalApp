2037603a4f91f8e2fc8672cc5cb030553377bbd6
lqr <1401219230@qq.com>
2020-10-20 20:34:48 +0800

    about us,finish

--- app/src/main/java/com/example/finalapp/MainActivity.java
+++ app/src/main/java/com/example/finalapp/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.finalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void onclick(View btn){
+        if(btn.getId()==R.id.about){
+            Intent in=new Intent(this,About.class);
+            startActivity(in);
+        }
     }
 }
