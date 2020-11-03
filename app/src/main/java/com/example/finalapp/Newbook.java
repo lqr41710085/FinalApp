768c28c8900073e7938922a19a8587dc881838e3
lqr <1401219230@qq.com>
2020-11-03 22:20:46 +0800

    beautify

--- app/src/main/java/com/example/finalapp/Newbook.java
+++ app/src/main/java/com/example/finalapp/Newbook.java
@@ -4,43 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.DatePicker;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.widget.Toast;
+
+
+import java.util.Calendar;
+
 
 
 public class Newbook extends AppCompatActivity {
 
     private static String TAG="Newbook";
+    DatePicker datePicker;
+    String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newbook);
+        datePicker=findViewById(R.id.date);
+        Calendar cal=Calendar.getInstance();
+        int y=cal.get(Calendar.YEAR),m=cal.get(Calendar.MONTH),d=cal.get(Calendar.DATE);//初始化为今天日期
+        datePicker.init(y, m, d, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    date=year+"-"+monthOfYear+"-"+dayOfMonth;
+                Log.i(TAG,"date::"+date);
+            }
+        });
+        Toast.makeText(this,"please choose date",Toast.LENGTH_LONG);
     }
     public void newbook(View view){
-        EditText datetext=findViewById(R.id.date);
-        String date;
-        if(!datetext.getText().toString().equals(""))
-            date=datetext.getText().toString();
-        else {
-            //设置日期
-            Date d=new Date(System.currentTimeMillis());
-            SimpleDateFormat format=new SimpleDateFormat(("yyyy/MM/dd"));
-            date=format.format(d);
-        }
-
-        if(view.getId()==R.id.outbtn){
-            Intent in=new Intent(this, Newout.class);
-            in.putExtra("date",date);
-            startActivity(in);
-        }
-        if(view.getId()==R.id.inbtn){
-            Intent in=new Intent(this, Newin.class);
-            in.putExtra("date",date);
-            startActivity(in);
-        }
-
+        Intent in=new Intent();
+        if(view.getId()==R.id.outbtn)
+            in=new Intent(this, Newout.class);
+        if(view.getId()==R.id.inbtn)
+            in=new Intent(this, Newin.class);
+        in.putExtra("date",date);
+        startActivity(in);
     }
 }
--- app/src/main/java/com/example/finalapp/searchdetail.java
+++ app/src/main/java/com/example/finalapp/searchdetail.java
@@ -26,42 +26,42 @@ public class searchdetail extends AppCompatActivity {
             TextView v=findViewById(R.id.datetext);
             v.setText(date);
             v=findViewById(R.id.notetext);
-            v.setText("Notes: "+cursor.getString(2));
+            v.setText(cursor.getString(2));
             v=findViewById(R.id.foodtext);
-            v.setText("food: "+cursor.getString(3));
+            v.setText(cursor.getString(3));
             v=findViewById(R.id.entertaintext);
-            v.setText("entertainment: "+cursor.getString(4));
+            v.setText(cursor.getString(4));
             v=findViewById(R.id.traffictext);
-            v.setText("traffic: "+cursor.getString(5));
+            v.setText(cursor.getString(5));
             v=findViewById(R.id.clothestext);
-            v.setText("clothes: "+cursor.getString(6));
+            v.setText(cursor.getString(6));
             v=findViewById(R.id.housetext);
-            v.setText("house: "+cursor.getString(7));
+            v.setText(cursor.getString(7));
             v=findViewById(R.id.studytext);
-            v.setText("study: "+cursor.getString(8));
+            v.setText(cursor.getString(8));
             v=findViewById(R.id.medicaltext);
-            v.setText("medical: "+cursor.getString(9));
+            v.setText(cursor.getString(9));
             v=findViewById(R.id.otherouttext);
-            v.setText("other: "+cursor.getString(10));
+            v.setText(cursor.getString(10));
             v=findViewById(R.id.allouttext);
             float all=cursor.getFloat(3)+cursor.getFloat(4)+cursor.getFloat(5)+
                 cursor.getFloat(6)+cursor.getFloat(7)+cursor.getFloat(8)+cursor.getFloat(9)+cursor.getFloat(10);
-            v.setText("all: "+all);
+            v.setText("all: "+all+" ");
 
             v=findViewById(R.id.wagetext);
-            v.setText("wage: "+cursor.getString(11));
+            v.setText(cursor.getString(11));
             v=findViewById(R.id.parttext);
-            v.setText("pattime: "+cursor.getString(12));
+            v.setText(cursor.getString(12));
             v=findViewById(R.id.packettext);
-            v.setText("redpacket: "+cursor.getString(13));
+            v.setText(cursor.getString(13));
             v=findViewById(R.id.otherintext);
-            v.setText("other: "+cursor.getString(14));
+            v.setText(cursor.getString(14));
             float all2=cursor.getFloat(11)+cursor.getFloat(12)+cursor.getFloat(13)+cursor.getFloat(14);
             v=findViewById(R.id.allintext);
-            v.setText("all: "+all2);
+            v.setText("all: "+all2+" ");
 
             v=findViewById(R.id.balancetext);
-            v.setText("Balance: "+(all2-all));
+            v.setText("Balance:"+(all2-all));
         }
 
     }
